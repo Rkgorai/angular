74505ef2d9aafaf37b420d5131ca8367765eadb0
Rahul Kishore Gorai <rahul.k.gorai@gmail.com>
2020-05-08 19:59:43 +0530

    Animations Part 2

--- src/app/dishdetail/dishdetail.component.ts
+++ src/app/dishdetail/dishdetail.component.ts
@@ -3,10 +3,11 @@ import { Dish } from '../shared/dish';
 import { Params, ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 import { DishService } from '../services/dish.service';
-import { switchMap } from 'rxjs/operators';
+import { switchMap, } from 'rxjs/operators';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Comment, CommentType } from '../shared/comment';
 import { trigger, state, style, animate, transition } from '@angular/animations';
+import { visibility, flyInOut, expand } from '../animations/app.animation';
 import { from } from 'rxjs';
 
 
@@ -15,18 +16,14 @@ import { from } from 'rxjs';
   selector: 'app-dishdetail',
   templateUrl: './dishdetail.component.html',
   styleUrls: ['./dishdetail.component.scss'],
-  animations: [
-    trigger('visibility', [
-      state('shown', style({
-        transform: 'scale(1.0)',
-        opacity: 1
-      })),
-      state('hidden', style({
-        transform: 'scale(0.5)',
-        opacity: 0
-      })),
-      transition('* => *', animate('0.5s ease-in-out'))
-    ])
+  host:{
+    '[@flyInOut]':'true',
+    'style': 'display: block;'
+  },
+  animations:[
+    flyInOut(),
+    visibility(),
+    expand()
   ]
 
 })
